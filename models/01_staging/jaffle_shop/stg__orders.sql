1cb9729a28cfa092cd2e036aea8a79e6b5188779
blegrand@decideom.fr <blegrand@decideom.fr>
2025-09-25 14:33:19 +0000

    co

--- models/01_staging/jaffle_shop/stg__orders.sql
+++ models/01_staging/jaffle_shop/stg__orders.sql
@@ -1,3 +1,6 @@
+{% if target.name=='default' %}
+{% set clause_where = 'Where Year(order_date)=Year(current_date())' %}
+{% endif %}
 
 select
     id as order_id,
@@ -5,3 +8,4 @@ select
     order_date,
     status,
 from {{ source('jaffle_shop', 'orders') }}
+{{clause_where}}
--- models/01_staging/stripe/stg__payments.sql
+++ models/01_staging/stripe/stg__payments.sql
@@ -4,6 +4,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    round( 1.0 * amount / 100, 4) as amount,
+   -- round( 1.0 * amount / 100, 4) as amount,
+     {{ cents_to_dollars('amount', 4) }} as amount,
     created as created_at,
 from {{ source('stripe', 'payment') }}
