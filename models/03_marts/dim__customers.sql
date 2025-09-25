20e5a3dd7ac710d90db0e2020acbf786304b1d25
blegrand@decideom.fr <blegrand@decideom.fr>
2025-09-25 12:18:49 +0000

    a

--- models/03_marts/dim__customers.sql
+++ models/03_marts/dim__customers.sql
@@ -19,6 +19,7 @@ select
     customers.customer_id,
     customers.first_name,
     customers.last_name,
+        lower(concat(customers.last_name,customers.first_name,'@jaffleshop.gg')) as email,
     customer_orders.first_order_date,
     customer_orders.most_recent_order_date,
     coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
--- tests/stg__payments__total_amount_positive.sql
+++ tests/stg__payments__total_amount_positive.sql
@@ -1,21 +1,18 @@
 select
-      count(*) as failures,
-      count(*) != 0 as should_warn,
-      count(*) != 0 as should_error
-    from (
-  
-
+    count(*) as failures,
+    count(*) != 0 as should_warn,
+    count(*) != 0 as should_error
+from (
 with all_values as (
 
     select
         order_id as value_field,
         sum(amount) as n_records
 
-    from analytics.dbt_lbenjamin_staging.stg__payments
+    from {{ ref('stg__payments') }}
     group by order_id
 
 )
-
 select *
 from all_values
 where n_records<0 
